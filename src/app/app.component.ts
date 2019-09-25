ba7f1582135c55f34d89f5372195f6ad5eb5b2e1
Jake <jakelovitz@gmail.com>
2019-09-25 11:59:26 -0400

    set up my first angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'away-team';
-}
+// export class AppComponent {
+//   title = 'away-team';
+// }
+
+export class AppComponent { todoArray=[]
+  deleteItem(){   console.log("delete item")  };
+  addTodo(value){this.todoArray.push(value)} };
